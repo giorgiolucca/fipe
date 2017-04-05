15db54758276ff4fe5a596e142b7714a311cc252
Giorgio Lucca <giorgiolucca@hotmail.com>
2017-04-05 14:15:30 -0300

    Remove comments

--- index.js
+++ index.js
@@ -6,12 +6,3 @@ module.exports = {
     sdk: require('./src/Fipe'),
     vehicleType: require('./src/VehicleTypes')
 };
-
-var fipe = require('./src/Fipe');
-
-// fipe.brand('motorcycle').findOneBy('name', 'harley').then(function(res) { console.log(res) });
-// fipe.brand('car').findBy('name', 'volks').then(function(res) { console.log(res); });
-// fipe.brand('car', 'fiat').then(function(res) { console.log(res); });
-// fipe.brand('car').all().then(function(res) { console.log(res); });
-// fipe.brand('motorcycle').findOneBy('name', 'harley').then(function(res) { console.log(res); });
-fipe.vehicle('car', 'chevrolet').findBy('name', 'Celta').then(function(res) { console.log(res); });
