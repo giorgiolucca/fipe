aff179a3b3d5b0a6b50b79258696a925ae4138df
Giorgio Lucca <giorgiolucca@hotmail.com>
2017-04-18 19:53:24 -0300

    More tests

--- test/unit/sdk.spec.js
+++ test/unit/sdk.spec.js
@@ -1,10 +1,8 @@
 'use strict';
 
 var expect = require('chai').expect;
-var nock = require('nock');
 var sprintf = require('sprintf').sprintf;
 var fipe = require('../../index');
-var path = require('path');
 var sdk = fipe.sdk;
 
 describe('fipe (unit)', function() {
@@ -21,30 +19,20 @@ describe('fipe (unit)', function() {
     });
 
     describe('brand method', function() {
-        beforeEach(function() {
-            nock('http://fipeapi.appspot.com/api/1')
-                .get('/carros/marcas.json')
-                .replyWithFile(200, path.join(__dirname, '/fixtures/all-brands.json'));
-        });
-
-        describe('when call SDK brand with one parameter (vehicleType)', function() {
-            it('should return a BrandRepository', function() {
-                expect(sdk.brand('car')).to.be.a('object');
-            });
-        });
-
         describe('when use SDK brand with one parameter (vehicleType) and call method all', function() {
-            it('should return a Promise', function() {
+            it('should return a Promise', function(done) {
                 expect(sdk.brand('car').all()).to.be.a('Promise');
+                done();
             });
         });
 
-        describe('when call SDK brand with two parameters (vehicleType, brandName)', function() {
-            it('should return a Promise', function() {
+        describe('when call SDK "car" brand with "brand name"', function() {
+            it('should return a Promise', function(done) {
                 expect(sdk.brand('car', 'fiat')).to.be.a('Promise');
+                done();
             });
 
-            it('should return info about "FIAT" car brand', function() {
+            it('should return info about "FIAT" car brand', function(done) {
                 sdk.brand('car', 'fiat').then(function(res) {
                     expect(res).to.deep.equal({
                         "name":"FIAT",
@@ -52,9 +40,73 @@ describe('fipe (unit)', function() {
                         "order":2,
                         "key":"fiat-21",
                         "id":21
-                    })
+                    });
+                    done();
+                });
+            });
+        });
+
+        describe('when call SDK "motorcycle" brand with "brand name"', function() {
+            it('should return a Promise', function(done) {
+                expect(sdk.brand('motorcycle', 'harley')).to.be.a('Promise');
+                done();
+            });
+
+            it('should return info about "HARLEY DAVIDSON" motorcycle brand', function(done) {
+                sdk.brand('motorcycle', 'harley').then(function(res) {
+                    expect(res).to.deep.equal({
+                        "name":"HARLEY-DAVIDSON",
+                        "fipe_name":"HARLEY-DAVIDSON",
+                        "order": 2,
+                        "key":"harley-davidson-77",
+                        "id": 77
+                    });
+                    done();
+                });
+            });
+        });
+
+        describe('when call SDK "truck" brand with "brand name"', function() {
+            it('should return a Promise', function(done) {
+                expect(sdk.brand('truck', 'scania')).to.be.a('Promise');
+                done();
+            });
+
+            it('should return info about "SCANIA" motorcycle brand', function(done) {
+                sdk.brand('truck', 'scania').then(function(res) {
+                    expect(res).to.deep.equal({
+                        "name":"SCANIA",
+                        "fipe_name":"SCANIA",
+                        "order": 1,
+                        "key":"scania-114",
+                        "id": 114
+                    });
+                    done();
                 });
             });
         });
     });
+
+    describe('vehicle method', function() {
+        describe('when call "getYearModel" method passing model ID and year', function() {
+            it('should return info about a car from brand "subaru"', function(done) {
+                sdk.vehicle('car', 'subaru').getYearModel('2125', '2004-2').then(function (res) {
+                    expect(res).to.deep.equal({
+                        "referencia": "abril de 2017",
+                        "fipe_codigo": "027039-3",
+                        "name": "Forester 2.0 16v 4x4 Turbo Aut.",
+                        "combustivel": "Gasolina",
+                        "marca": "Subaru",
+                        "ano_modelo": "2004",
+                        "preco": "R$ 26.610,00",
+                        "key": "forester-2004",
+                        "time": 0,
+                        "veiculo": "Forester 2.0 16v 4x4 Turbo Aut.",
+                        "id": "2004"
+                    });
+                    done();
+                });
+            });
+        });
+    })
 });
